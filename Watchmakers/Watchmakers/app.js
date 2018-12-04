ec513413c6222d71d0446ae1d12981d0563ea1a6
Michał Dela <dela_m@interia.pl>
2018-12-04 10:47:51 +0100

    Project cleaned from .js and .map files. All js and map files are now beeing created in ./dist/ folder

--- Watchmakers/Watchmakers/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const PORT = 1337;
-server_1.default.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=app.js.map
